16ef446726c35b15cae903ea5240fe4fe189e11f
unknown <m.verbruggen_2@tilburguniversity.edu>
2021-09-24 13:03:00 +0200

    Change of datacleaning file to get output csv file in correct directory (gen/datapreparation/audit)

--- src/datapreparation/Datacleaning.R
+++ src/datapreparation/Datacleaning.R
@@ -15,10 +15,9 @@ listings_merged <- listings_merged %>%
     filter(price != 0)
 
 # Removing columns
+
 cols_to_drop = c('id', 'name', 'host_id', 'host_name', 'neighbourhood_group', 'neighbourhood', 'latitude', 'longitude', 'room_type', 'minimum_nights', 'number_of_reviews', 'last_review', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365' )
 listings_merged_tiny = listings_merged[, which(!colnames(listings_merged)%in%cols_to_drop)]
 
 # Save as file
-fwrite(listings_merged_tiny, "output/listings_cleaned.csv")
-
-listings_merged_tiny <- 1
+fwrite(listings_merged_tiny, "gen/datapreparation/audit/listings_cleaned.csv")
