6b3cf64225cd7b6a0ba9e5197c3d2220176019cc
teungeurts <t.a.g.m.geurts@tilburguniversity.edu>
2021-09-24 11:40:30 +0200

    new structure

---
+++ src/datapreparation/Datacleaning.R
@@ -0,0 +1,24 @@
+#load packages
+
+library(tidyverse)
+library(dplyr)
+library(data.table)
+
+# Loading datafile
+listings_merged <- read.csv("input/listings_merged.csv")
+
+# Check for outliers
+summary(listings_merged)
+
+# Removed the 3 listings with a price of 0 dollars
+listings_merged <- listings_merged %>%
+    filter(price != 0)
+
+# Removing columns
+cols_to_drop = c('id', 'name', 'host_id', 'host_name', 'neighbourhood_group', 'neighbourhood', 'latitude', 'longitude', 'room_type', 'minimum_nights', 'number_of_reviews', 'last_review', 'reviews_per_month', 'calculated_host_listings_count', 'availability_365' )
+listings_merged_tiny = listings_merged[, which(!colnames(listings_merged)%in%cols_to_drop)]
+
+# Save as file
+fwrite(listings_merged_tiny, "output/listings_cleaned.csv")
+
+listings_merged_tiny <- 1
---
+++ src/datapreparation/merge_files.R
@@ -0,0 +1,29 @@
+# Load Libraries
+library(readr)
+library(jsonlite)
+library(knitr)
+library(dplyr)
+library(tidyverse)
+
+# Load Data
+listings0820 <- read.csv("data/listings0820.csv")
+listings0821 <- read.csv("data/listings0821.csv")
+
+#Create dummy variable 'regulation' in both datasets. 
+# Dummy variable = 0 for dataset before regulation
+# Dummy variable = 1 for dataset after regulation
+
+listings0820$regulation <- 0
+listings0821$regulation <- 1
+
+#Merge datasets together to be able to perform analysis
+
+listings_merged <- merge(listings0820, listings0821, all.x=TRUE, all.y=TRUE)
+
+#Make dummy variable a factor 
+
+listings_merged$regulation <- as.factor(listings_merged$regulation)
+
+# Write CSV file
+
+write.csv(listings_merged, "gen/datapreparation/audit/listings_merged.csv", row.names = FALSE)
