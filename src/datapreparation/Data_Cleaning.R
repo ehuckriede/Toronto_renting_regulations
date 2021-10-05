03cc25dc6835373dfef08d62e4c1eae5cd51a718
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-05 13:33:01 +0200

    Changed price variable from character to numeric so that we can perform a regression analysis

--- src/datapreparation/Data_Cleaning.R
+++ src/datapreparation/Data_Cleaning.R
@@ -9,7 +9,14 @@ summary(all_data)
 
 # Remove the 3 listings with a price of 0 dollars
 all_data <- all_data %>%
-    filter(price != 0)
+    filter(price != "$0.00")
+
+# Transforming price so that it can be a numerical variable instead of character
+
+all_data$price2 <- (gsub("\\,", "", all_data$price))
+all_data$price2 <- (gsub("\\$", "", all_data$price2))
+all_data$price2 <- as.numeric(all_data$price2)
+sum(is.na(all_data$price2))
 
 # Removing columns
 
@@ -17,7 +24,9 @@ all_data_cleaned <- all_data[ -c(1:39, 41:74) ]
 summary(all_data_cleaned)
 
 # Checking for missing values
+
 sum(is.na(all_data_cleaned))
 
 # Write output
+
 write.csv(all_data_cleaned, "../../data/Cleaned_data.csv", row.names = FALSE)
