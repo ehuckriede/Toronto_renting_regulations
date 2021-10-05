081a985fc7b51e57ed6b60fde1513939020fd0fd
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-05 13:53:03 +0200

    the logistic regression function without errors

--- src/analysis/Analysis_data.R
+++ src/analysis/Analysis_data.R
@@ -2,10 +2,10 @@
 library(dplyr)
 
 # Load the data
-Cleaned_data <- read.csv(file = '../../data/Cleaned_data.csv')
+cleaned_data <- read.csv(file = '../../data/Cleaned_data.csv')
 
 # Summarize data and state what types of variables we have
-summary(Cleaned_data)
+summary(cleaned_data)
 # price = DV = metric
 # regulations = IV = metric
 # Control variables (all metric):
@@ -13,3 +13,5 @@ summary(Cleaned_data)
 ## Type of rooms: Entire_room_apt, Hotel_room, Private_room & Shared_room
 
 # Run analysis with regression
+price_lm <- lm(price ~ regulation + Old_Town + Etobicoke + North_York + East_York + York + Scarborough + Entire_home_apt + Hotel_room + Private_room + Shared_room, cleaned_data)
+summary(price_lm)
