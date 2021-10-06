d7d244ad5e9c80b01e735a33ac3eaf06fde41cb8
Ilse <i.c.m.vdnbosch@tilburguniversity.ed>
2021-10-06 11:38:09 +0200

    Continued on the analysis. Among others, set one of the neighbourhood dummy variables to 0 and one of the dummy variables of room type

--- src/analysis/Analysis_data.R
+++ src/analysis/Analysis_data.R
@@ -2,7 +2,7 @@
 library(dplyr)
 
 # Load the data
-cleaned_data <- read.csv(file = '../../data/Cleaned_data.csv')
+cleaned_data <- read.csv(file = '../../gen/data-preparation/output/cleaned_data.csv')
 
 # Summarize data and state what types of variables we have
 summary(cleaned_data)
@@ -12,6 +12,10 @@ summary(cleaned_data)
 ## Neighbourhoods: Old_Town, Etobicoke, North_York, East_York, York & Scarborough
 ## Type of rooms: Entire_room_apt, Hotel_room, Private_room & Shared_room
 
+# Set one dummy variable of the neighbourhoods to zero and one of the roomtypes to zero since the values of all neighbourhoods and roomtypes will always add up to 1
+cleaned_data$scarborough <- 0
+cleaned_data$shared_room <- 0
+
 # Run analysis with regression
-price_lm <- lm(price ~ regulation + Old_Town + Etobicoke + North_York + East_York + York + Scarborough + Entire_home_apt + Hotel_room + Private_room + Shared_room, cleaned_data)
+price_lm <- lm(price ~ regulation + old_town + etobicoke + north_york + east_york + york + scarborough + entire_home_apt + hotel_room + private_room + shared_room, cleaned_data)
 summary(price_lm)
