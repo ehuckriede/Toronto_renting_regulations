2d35770dff6308391e0c76a60de7af95dfa01307
ehuckriede <82393051+ehuckriede@users.noreply.github.com>
2021-10-06 16:14:13 +0200

    Finished first figure

--- src/analysis/figures.R
+++ src/analysis/figures.R
@@ -17,37 +17,11 @@ roomtype_price <- all_data %>%
   summarise(price = mean(price))
 
 roomtype_price %>%
-  ggplot(aes(x = room_type, y = price)) +
-  geom_point()
-
-all_data %>%
-  group_by(room_type) %>%
-  summarise(price = mean(price)) %>%
-  ungroup() %>%
-  pivot_wider(names_from = regulation, values_from = price) %>%
-  ggplot(aes(x = ))
-
-  geom_bar()
-
-treat_table <- paidsearch %>%
-  group_by(treatment_period, treatment) %>%
-  summarise(revenue = mean(revenue)) %>%
-  ungroup()
-
-treat_table %>%
-  pivot_wider(names_from = treatment, values_from = revenue) %>%
-  mutate_all(funs(. - lag(.))) %>%
-  mutate(did_simple = "1" - "0") %>%
-  na.omit() %>%
-  select(did_simple)
-
-
+  ggplot(aes(x = room_type, y = price, fill = regulation)) +
+  geom_bar(stat = "identity", position = "dodge2") +
+  xlab("Room Type") +
+  ylab("Price per Night") +
+  ggtitle("Price per Room Type", subtitle = "Before and After Regulation") +
+  guides(fill = guide_legend(title = "Regulation")) +
+  theme_minimal()
   
-ggplot(aes(x = room_type, y = price, fill = regulation)) +
-  geom_bar() 
-
-movies %>%    
-  group_by(genre, vote) %>%
-  count(vote) %>%
-  ggplot(aes(x = genre, y = n, fill = vote)) +
-  geom_bar(position = "fill", stat = "identity") +
