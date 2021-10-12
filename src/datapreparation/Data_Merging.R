af952b53deabfa06403acf626adbf77214f48337
Ilse <i.c.m.vdnbosch@tilburguniversity.ed>
2021-10-12 14:40:01 +0200

    Removed line which changed the regulations variable from character to factor since it should be a numeric variable

--- src/datapreparation/Data_Merging.R
+++ src/datapreparation/Data_Merging.R
@@ -10,17 +10,10 @@ listings0820$regulation <- 0
 listings0821$regulation <- 1
 
 #Merge datasets together to be able to perform analysis
-
 listings_merged <- merge(listings0820, listings0821, all.x=TRUE, all.y=TRUE)
 
-#Make dummy variable a factor 
-
-listings_merged$regulation <- as.factor(listings_merged$regulation)
-
 # Write output
-
 dir.create('../../gen')
 dir.create('../../gen/data-preparation')
 dir.create('../../gen/data-preparation/input')
 write.csv(listings_merged, "../../gen/data-preparation/input/merged_data.csv", row.names = FALSE)
-
