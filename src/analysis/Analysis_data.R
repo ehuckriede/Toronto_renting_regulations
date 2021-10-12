3c37ab20b31ba2cc90dcd779cfafa19dc8091a5b
Ilse <i.c.m.vdnbosch@tilburguniversity.ed>
2021-10-12 14:45:13 +0200

    Removed the line which changed the regulations variable from being a character to a factor, since it has to be a numeric variable.

--- src/analysis/Analysis_data.R
+++ src/analysis/Analysis_data.R
@@ -4,9 +4,6 @@ library(dplyr)
 # Load the data
 cleaned_data <- read.csv(file = '../../gen/data-preparation/output/cleaned_data.csv')
 
-# Make dummy variable a factor
-cleaned_data$regulation <- as.factor(cleaned_data$regulation)
-
 # Summarize data and state what types of variables we have
 summary(cleaned_data)
 # price = DV = metric
@@ -14,6 +11,10 @@ summary(cleaned_data)
 # Control variables (all metric):
 ## Neighbourhoods: Old_Town, Etobicoke, North_York, East_York, York & Scarborough
 ## Type of rooms: Entire_room_apt, Hotel_room, Private_room & Shared_room
+
+# Run analysis with regression
+price_lm <- lm(price ~ regulation + old_town + etobicoke + north_york + east_york + york + entire_home_apt + hotel_room + private_room, cleaned_data)
+summary(price_lm)
 
 # Filter on regulations
 cleaned_data_1 <- cleaned_data %>%
@@ -24,7 +25,3 @@ sum(cleaned_data_0$old_town) - sum(cleaned_data_1$old_town)
 sum(cleaned_data_0$etobicoke) - sum(cleaned_data_1$etobicoke)
 sum(cleaned_data_0$north_york) - sum(cleaned_data_1$north_york)
 sum(cleaned_data_0$entire_home_apt) - sum(cleaned_data_1$entire_home_apt)
-
-# Run analysis with regression
-price_lm <- lm(price ~ regulation + old_town + etobicoke + north_york + east_york + york + entire_home_apt + hotel_room + private_room, cleaned_data)
-summary(price_lm)
