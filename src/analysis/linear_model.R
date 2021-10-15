7dec7bbc3878dc0837132f7a4ec9a671c3cb3785
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-15 15:23:42 +0200

    Added the other three dummy variables (last three columns) to the linear regression model

--- src/analysis/linear_model.R
+++ src/analysis/linear_model.R
@@ -12,13 +12,13 @@ summary(cleaned_data)
 # Control variables (all metric):
 ## Neighbourhoods: Old_Town, Etobicoke, North_York, East_York, York & Scarborough
 ## Type of rooms: Entire_room_apt, Hotel_room, Private_room & Shared_room
-
-
+## Host since: before May 19 2016 and after May 19 2016
+## Superhost status: yes or no
+## Host identity verified: yes no 
 
 # Run analysis with regression
-price_lm <- lm(price ~ regulation + old_town + etobicoke + north_york + east_york + york + entire_home_apt + hotel_room + private_room, cleaned_data)
+price_lm <- lm(price ~ regulation + old_town + etobicoke + north_york + east_york + york + entire_home_apt + hotel_room + private_room + host_since_dummy + superhost_dummy + identity_verified_dummy, cleaned_data)
 summary(price_lm)
 
 dir.create('../../gen/analysis/output/', recursive = T)
 sink("../../gen/analysis/output/linear_model_output.txt")
-print(summary(price_lm))
