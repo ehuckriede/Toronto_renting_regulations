4869a79d74265f4072a1b63d8dac442b9fd33875
unknown <m.verbruggen_2@tilburguniversity.edu>
2021-10-16 14:01:48 +0200

    Two new R-files to create barplot for change in prices

---
+++ src/analysis/create_prices_barplot.R
@@ -0,0 +1,24 @@
+# --- Load Libraries --- #
+library(ggplot2)
+library(dplyr)
+library(gapminder)
+library(tidyverse)
+library(knitr)
+library(recommenderlab)
+library(RColorBrewer)
+
+# --- Load Data --- #
+regulation_price <- read.csv(file = '../../gen/analysis/input/data_for_barplot.csv')
+regulation_price$regulation <- as.factor(regulation_price$regulation)
+
+
+pdf("../../gen/analysis/output/prices_barplot.pdf")
+regulation_price %>% 
+  ggplot(aes(x=regulation, y=price, fill = regulation)) +
+  geom_bar(stat = "identity", position = "dodge2") + 
+  xlab("Before or After Regulation") + ylab("Mean Price") + 
+  ggtitle("Mean Price before and after regulation") + 
+  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) + 
+  guides(fill = guide_legend(title = "Regulation")) + 
+  scale_fill_discrete(labels = c("Before Regulation", "After Regulation")) + geom_col()
+dev.off()
---
+++ src/analysis/get_data_for_barplot.R
@@ -0,0 +1,15 @@
+# --- Load Libraries --- #
+library(dplyr)
+library(haven)
+
+# --- Load Data --- #
+all_data <- read.csv(file = '../../gen/data-preparation/output/cleaned_data.csv')
+
+# --- Data for Plot 3 --- #
+
+regulation_price <- all_data %>%
+  group_by(regulation) %>%
+  summarise(price = mean(price))
+
+write.csv(regulation_price, "../../gen/analysis/input/data_for_barplot.csv", row.names = FALSE)
+
