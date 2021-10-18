f2b1b27445d58d758caf49f3fb681cbe906a8c01
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-18 09:45:08 +0200

    Changed the output from CSV to pdf

--- src/analysis/get_data_for_table_dummies.R
+++ src/analysis/get_data_for_table_dummies.R
@@ -7,11 +7,13 @@ host_data <- read.csv(file = '../../gen/data-preparation/output/cleaned_data.csv
 
 # --- Table 1 --- #
 
+# Make datasetd
 host_price <- host_data %>%
   group_by(host_since_dummy, superhost_dummy, regulation) %>%
   summarise(price = mean(price)) 
 
+# Create table 
+pdf("../../gen/analysis/output/table_host_dummies.pdf")
 gridExtra::grid.table(host_price)
+dev.off()
 
-
-write.csv(host_price, "../../gen/analysis/output/table_host_dummies.csv", row.names = FALSE)
