f0ff32d1dcd250b7eb87d5bcf2db7acb89bc9131
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-18 10:41:28 +0200

    Removed unneccesary colums

--- src/analysis/get_data_for_table_dummies.R
+++ src/analysis/get_data_for_table_dummies.R
@@ -12,6 +12,9 @@ host_price <- host_data %>%
   group_by(host_since_dummy, superhost_dummy, regulation) %>%
   summarise(price = mean(price)) 
 
+# Remove irrelevant columns 
+host_price <- host_price[-c(1:2, 7:8),]
+
 # Create table 
 pdf("../../gen/analysis/output/table_host_dummies.pdf")
 gridExtra::grid.table(host_price)
