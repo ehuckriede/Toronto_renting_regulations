dc57e75ecfa61881ded6b3ee99108f8f9f83b798
juliehabets <j.n.g.habets@tilburguniversity.edu>
2021-10-18 08:47:41 +0200

    Changed plot number

--- src/analysis/get_data_for_barplot.R
+++ src/analysis/get_data_for_barplot.R
@@ -5,7 +5,7 @@ library(haven)
 # --- Load Data --- #
 all_data <- read.csv(file = '../../gen/data-preparation/output/cleaned_data.csv')
 
-# --- Data for Plot 3 --- #
+# --- Data for Plot 1 --- #
 
 regulation_price <- all_data %>%
   group_by(regulation) %>%
